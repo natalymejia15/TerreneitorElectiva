abb6f8f53e6222b2d9d48e5801d9e69f7f5ec5f5
Nataly <practicante_planea3@corantioquia.gov.co>
2024-04-17 14:43:32 -0500

    ADD The navbar and product detail page begins to be created

---
+++ src/products/helpers/ProductDetail.js
@@ -0,0 +1,9 @@
+export const productData = {
+  image: 'https://example.com/product-image.jpg',
+  title: 'Producto XYZ',
+  description: 'Este es un producto increíble.',
+  comments: [
+    { id: 1, text: '¡Me encanta este producto!', author: 'Usuario 1' },
+    { id: 2, text: 'Muy buena calidad.', author: 'Usuario 2' },
+  ],
+};
