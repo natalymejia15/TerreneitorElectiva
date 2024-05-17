403b370a5b34c5b372bca554dc9d7d4b40663787
Nataly <practicante_planea3@corantioquia.gov.co>
2024-05-17 16:45:37 -0500

    CH Corrections visual

--- src/products/components/HeaderProduct.jsx
+++ src/products/components/HeaderProduct.jsx
@@ -1,57 +1,86 @@
-import { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom';
-import { FirebaseDB } from '~firebase/config';
-import { collection, query, getDocs, orderBy, limit } from "firebase/firestore/lite";
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { FirebaseDB } from "~firebase/config";
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy,
+  limit,
+} from "firebase/firestore/lite";
 import { FaThumbsUp } from "react-icons/fa";
 
 export const HeaderProduct = () => {
+  const [maxRate, setMaxRate] = useState([]);
 
-  const [maxRate, setMaxRate]= useState([]);
-
-  const getProductRate=async ()=>{
-    const queryRates = query(collection(FirebaseDB, 'products'), orderBy('rate', 'desc'),limit(1));
+  const getProductRate = async () => {
+    const queryRates = query(
+      collection(FirebaseDB, "products"),
+      orderBy("rate", "desc"),
+      limit(1)
+    );
     const querySnapshot = await getDocs(queryRates);
-    const docs=[]
+    const docs = [];
     querySnapshot.forEach((doc) => {
       docs.push({ ...doc.data(), id: doc.id });
-     });
+    });
     setMaxRate(docs);
     console.log(docs);
-  }
+  };
 
-  useEffect(()=>{
-      getProductRate();
-  }, [])
+  useEffect(() => {
+    getProductRate();
+  }, []);
 
   return (
     <>
       {maxRate.map((products) => (
-         <div key={products.id}  className="md:flex max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl m-3">
-            <div className="md:flex-shrink-0">
-                <img className="h-48 w-full object-cover md:w-48" src={ products.image } alt=""/>
+        <div
+          key={products.id}
+          className="md:flex max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl m-3"
+        >
+          <div className="md:flex-shrink-0">
+            <img
+              className="h-48 w-full object-cover md:w-48"
+              src={products.image}
+              alt=""
+            />
+          </div>
+          <div className="p-8 ml-4">
+            <NavLink
+              to={`/products/${products.id}`}
+              className="uppercase tracking-wide font-semibold text-violet-600 hover:text-violet-900"
+            >
+              {products.name}
+            </NavLink>
+
+            <a
+              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+              href="#"
+            >
+              {products.category}
+            </a>
+            <p className="mt-2 text-gray-500">{products.description}</p>
+            <div className="flex items-center">
+              <p className="tracking-wide text-sm text-violet-900 font-semibold">
+                Higher Rate {products.rate}{" "}
+              </p>
+              <FaThumbsUp className="ml-2" />
             </div>
-            <div className="p-8 ml-4">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{ products.name }</div>
-                  <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline" href="#" >{ products.category}</a>
-                  <p className="mt-2 text-gray-500">{ products.description}</p>
-                  <div className="flex items-center">
-                    <p className="tracking-wide text-sm text-violet-900 font-semibold">Higher Rate { products.rate } </p>
-                    <FaThumbsUp className="ml-2" />
-                  </div>
-                <br />
-                <div className="flex justify-center">
-                  <NavLink
-                    to={products.url} 
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    className='bg-violet-800 hover:bg-gray-400 text-white font-bold py-2 px-4 border border-violet-800 rounded'
-                  >
-                      Get Product
-                  </NavLink>
-                </div>                
+            <br />
+            <div className="flex justify-center">
+              <NavLink
+                to={products.url}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="bg-violet-800 hover:bg-gray-400 text-white font-bold py-2 px-4 border border-violet-800 rounded"
+              >
+                Get Product
+              </NavLink>
             </div>
-        </div> 
-      ))}   
+          </div>
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
--- src/products/components/ProductItem.jsx
+++ src/products/components/ProductItem.jsx
@@ -1,7 +1,6 @@
-import React  from 'react'
-import { HandleUpvote } from './HandleUpvote';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { HandleUpvote } from "./HandleUpvote";
+import { NavLink } from "react-router-dom";
 
 export const ProductItem = ({
   name,
@@ -12,34 +11,39 @@ export const ProductItem = ({
   id,
   displayName,
 }) => {
-  const firstLetter = name.charAt(0).toUpperCase();   
+  const firstLetter = name.charAt(0).toUpperCase();
   const [upvotes, setUpvotes] = React.useState(0);
 
   const handleUpvoteChange = (value) => {
-      setUpvotes(value); 
-  }
+    setUpvotes(value);
+  };
 
   return (
-    <div className="md:flex max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl m-3">
+    <div className="md:flex max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl m-3 relative">
       {image ? (
         <div className="flex items-center">
           <div className="w-20 h-24 overflow-hidden ">
-            <img className="w-full h-full object-cover" alt={firstLetter} src={image} />
+            <img
+              className="w-full h-full object-cover"
+              alt={firstLetter}
+              src={image}
+            />
           </div>
           <div className="flex-1 p-4">
             <div className="uppercase tracking-wide font-semibold text-violet-600 hover:text-blue-600">
-            <NavLink
-              to={`/products/${id}`}
-              className="uppercase tracking-wide font-semibold text-violet-600 hover:text-violet-900"
-            >
-              {name}
-            </NavLink>
+              <NavLink
+                to={`/products/${id}`}
+                className="uppercase tracking-wide font-semibold text-violet-600 hover:text-violet-900"
+              >
+                {name}
+              </NavLink>
             </div>
             <p className="mt-2 text-base text-neutral-600 dark:text-neutral-200">
               {description}
             </p>
             <div>
-              {displayName }{rate}
+              <p className="text-gray-600">Rate: {rate}</p>
+              <p className="text-gray-600">User: {displayName}</p>
             </div>
           </div>
         </div>
@@ -50,23 +54,25 @@ export const ProductItem = ({
           </div>
           <div className="flex-1 p-4">
             <div className="uppercase tracking-wide font-semibold text-violet-600 hover:text-violet-700">
-            <a href={url} target="_blank">{name}</a>
+              <a href={url} target="_blank">
+                {name}
+              </a>
             </div>
             <p className="mt-2 text-base text-neutral-600 dark:text-neutral-200">
               {description}
             </p>
             <div>
-              {displayName }{rate}
-            </div>            
+              <p className="text-gray-600">Rate: {rate}</p>
+              <p className="text-gray-600">User: {displayName}</p>
+            </div>
           </div>
         </div>
       )}
-      <div className="bg-violet-500 hover:bg-violet-400 text-gray-300 font-bold mb-12 py-2 px-4 rounded inline-flex items-center">
-        <HandleUpvote
-          id={userId}
-          onUpvoteChange={handleUpvoteChange} 
-        />
-        {upvotes}
+      <div className="absolute top-2 right-2">
+        <div className="bg-violet-500 hover:bg-violet-400 text-gray-300 font-bold py-2 px-4 rounded inline-flex items-center">
+          <HandleUpvote id={userId} onUpvoteChange={handleUpvoteChange} />
+          {upvotes}
+        </div>
       </div>
     </div>
   );
