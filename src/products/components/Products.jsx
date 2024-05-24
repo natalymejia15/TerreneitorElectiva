830770dfe75d85ec3b8999d104bdebd49723d3a0
Alexander <jhon.montano0327@gmail.com>
2024-05-23 21:30:39 -0500

    Update page profile where it´s add produts

--- src/products/components/Products.jsx
+++ src/products/components/Products.jsx
@@ -4,43 +4,41 @@ import { collection, getDocs, query, where, orderBy } from "firebase/firestore/l
 import { FirebaseDB } from "~firebase/config";
 import { ProductItemLogin } from "./ProductItemLogin";
 
-export const Products = (props) => {
+export const Products = ({ userId, showProducts, showHunter }) => {
   const [products, setProducts] = useState([]);
   const { user } = useContext(AuthContext);
-  const showProducts = props.show;
-  const showHunter=props.showHunter;
 
   useEffect(() => {
     const getList = async () => {
       try {
-          let queryProduct;
-          if (!showHunter){
-            const fieldName = 'userId'; 
-            const searchValue = user.uid;
-            queryProduct = query(collection(FirebaseDB, 'products'), where(fieldName, '==', searchValue));
-          } else {
-            const orderByField = 'userId'; 
-            queryProduct = query(collection(FirebaseDB, 'products'), orderBy(orderByField));
-          }
-          const querySnapshot = await getDocs(queryProduct);
-          const docs = [];
-          querySnapshot.forEach((doc) => {
-            docs.push({ ...doc.data(), id: doc.id });
-           });
+        let queryProduct;
+        if (showHunter) {
+          queryProduct = query(collection(FirebaseDB, 'products'), orderBy('userId'));
+        } else {
+          const fieldName = 'userId';
+          const searchValue = userId || user.uid;
+          queryProduct = query(collection(FirebaseDB, 'products'), where(fieldName, '==', searchValue));
+        }
+        const querySnapshot = await getDocs(queryProduct);
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id });
+        });
         setProducts(docs);
       } catch (error) {
         console.log(error);
       }
     };
+
     getList();
-  }, []);
- 
+  }, [userId, user, showHunter]);
+
   return (
     <div className="mr-10 ml-10 mb-2">
       <ul>
-        {products.map((products) => (
-          <div key={products.id}>
-            <ProductItemLogin show={showProducts} {...products} />
+        {products.map((product) => (
+          <div key={product.id}>
+            <ProductItemLogin show={showProducts} {...product} />
             <hr className="my-4 border-t" />
           </div>
         ))}
